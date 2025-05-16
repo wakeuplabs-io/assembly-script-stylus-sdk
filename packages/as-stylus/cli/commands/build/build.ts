ae9f6ce2d9007232946a09e3eccb2ce425c6c027
juandahl <juandahl20@gmail.com>
2025-05-16 18:33:21 -0300

    feat: build all the contracts

--- packages/as-stylus/cli/commands/build/build.ts
+++ packages/as-stylus/cli/commands/build/build.ts
@@ -1,39 +1,79 @@
 import fs from "fs";
 import path from "path";
 import { applyAnalysis } from "./analyzers/index.js";
-import { IRContract } from "../../types/ir.types.js";
-export function runBuild() {
+import { IRContract } from "@/cli/types/ir.types.js";
 
-  // const projectRoot   = process.cwd();}
-
-  // const userIndexPath = fs.existsSync(path.resolve(projectRoot, "index.ts"))
-  // ? path.resolve(projectRoot, "index.ts")
-  // : fallbackContractPath;
-
-  const fallbackProjectRoot = path.resolve("/Users/francoperez/repos/wakeup/assembly-script-stylus-sdk/packages/contracts/test");
-  const fallbackContractPath = path.resolve("/Users/francoperez/repos/wakeup/assembly-script-stylus-sdk/packages/contracts/test/contract.ts");
-  const userIndexPath = fallbackContractPath;
-
-  if (!fs.existsSync(userIndexPath)) {
+function getAllProjects(contractsRoot: string) {
+  const folders = fs.readdirSync(contractsRoot);
+  if (folders.length === 0) {
     console.error(
-      `[as‑stylus] Error: cannot find "index.ts" in ${userIndexPath}.
-       Make sure you run "npx as-stylus build" inside a contract
-       folder that contains an AssemblyScript entry file named contract.ts.`
+      `[as‑stylus] Error: No projects found.\n Make sure you have a project in the contracts folder.`
     );
     process.exit(1);
   }
 
-  const targetPath = path.join(fallbackProjectRoot, ".dist");
-  if (fs.existsSync(targetPath)) fs.rmSync(targetPath, { recursive: true, force: true });
-  fs.mkdirSync(targetPath, { recursive: true });
-  const transformedPath = path.join(targetPath, "contract.transformed.ts");
-  fs.copyFileSync(userIndexPath, transformedPath);
+  return folders.reduce((acc: string[], folder: string) => {
+    if (folder.startsWith(".")) {
+      return acc;
+    }
 
-  const contract: IRContract = applyAnalysis(transformedPath) 
-  // transformFromIR(contract, path.dirname(transformedPath));
-  // buildProject(userIndexPath, contract);
+    if (fs.statSync(path.join(contractsRoot, folder)).isDirectory()) {
+      return [...acc, path.join(contractsRoot, folder)];
+    }
 
-  console.log(`Generated new contract project at: ${targetPath}`);
+    return acc;
+  }, []);
+}
+
+function getAllContractPaths(projectRoot: string) {
+  const contractPaths = fs.readdirSync(projectRoot);
+  const projectName = projectRoot.split("/").pop();
+  if (contractPaths.length === 0) {
+    console.error(
+      `
+        [as‑stylus] Error: No contracts found.
+        Make sure you have a [file].ts in the ${projectName} folder.
+      `
+    );
+    process.exit(1);
+  }
+
+  return contractPaths.reduce((acc: string[], contractPath: string) => { 
+    if (contractPath.endsWith(".ts")) {
+      return [...acc, path.join(projectRoot, contractPath)];
+    }
+    return acc;
+  }, []);
+}
+
+export function runBuild() {
+  const contractsRoot = path.resolve(process.cwd(), "../contracts");
+  const projects = getAllProjects(contractsRoot);
+
+  projects.forEach((project) => {
+    const contractPaths = getAllContractPaths(project);
+    const projectName = project.split("/").pop()!;
+
+    contractPaths.forEach(async (contractPath) => {
+      const projectTargetPath = path.join(contractsRoot, ".dist", projectName);
+      const contractName = contractPath.split("/").pop()!;
+      const transformedPath = path.join(projectTargetPath, `${contractName.replace(".ts", "")}.transformed.ts`);
+
+      if (!fs.existsSync(projectTargetPath)) {
+        console.log("Creating project", project);
+        fs.mkdirSync(projectTargetPath, { recursive: true });
+      }
+      console.log(contractPath, transformedPath);
+      fs.copyFileSync(contractPath, transformedPath);
+
+      const contract: IRContract = applyAnalysis(transformedPath) 
+      // transformFromIR(contract, path.dirname(transformedPath));
+      // buildProject(userIndexPath, contract);
+    
+      console.log(`Generated new contract project at: ${projectTargetPath}`);
+    }
+  )
+  });
 }
 
 runBuild();
