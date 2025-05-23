ff04e7b34b9328c57ea10e784c1cb761ef544d76
juandahl <juandahl20@gmail.com>
2025-05-23 15:41:08 -0300

    improvement: code improvement on validators

--- packages/as-stylus/cli/commands/build/analyzers/if/syntax-validator.ts
+++ packages/as-stylus/cli/commands/build/analyzers/if/syntax-validator.ts
@@ -2,6 +2,7 @@ import { IfStatement, SyntaxKind } from "ts-morph";
 
 import { BaseValidator } from "../shared/base-validator.js";
 import { ErrorManager } from "../shared/error-manager.js";
+import { BaseValidator } from "../shared/base-validator.js";
 
 const ERROR_MESSAGES = {
   CONDITION_NOT_BOOLEAN: (conditionType: string) => `If condition must be a boolean expression, got ${conditionType}`,
