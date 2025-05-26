0395b923da90724e8c97d5079303bd14463038e7
francoperez03 <franco.perez03@gmail.com>
2025-05-26 15:38:38 -0300

    feat: uint256 args support added

--- packages/as-stylus/templates/index.template.ts
+++ packages/as-stylus/templates/index.template.ts
@@ -5,6 +5,7 @@
 
 import { __keep_imports } from "../../../as-stylus/core/modules/keep-imports";
 import { read_args, write_result } from "../../../as-stylus/core/modules/hostio";
+import { initHeap } from "../../../as-stylus/core/modules/memory";
 
 // @logic_imports
 
@@ -13,6 +14,7 @@ __keep_imports(false);
 export function user_entrypoint(args_len: usize): i32 {
   const position = memory.grow(<i32>((args_len + 0xffff) >> 16));
   read_args(position);
+  initHeap(position, args_len);
   const selector: u32 =
   (<u32>load<u8>(0) << 24) |
   (<u32>load<u8>(1) << 16) |
