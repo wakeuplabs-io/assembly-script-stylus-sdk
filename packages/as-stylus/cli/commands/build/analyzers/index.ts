8c6bf3d6329605f271b630b87a096f60e7f555d9
juandahl <juandahl20@gmail.com>
2025-05-27 09:24:16 -0300

    fix: minor changes in linter

--- packages/as-stylus/cli/commands/build/analyzers/index.ts
+++ packages/as-stylus/cli/commands/build/analyzers/index.ts
@@ -4,9 +4,9 @@ import { Project } from "ts-morph";
 
 import { IRContract } from "@/cli/types/ir.types.js";
 
-import { exportContractToJSON, generateContractTree } from "./tree-builder.js";
-import { ErrorManager } from "./shared/error-manager.js";
 import { ContractIRBuilder } from "./contract/ir-builder.js";
+import { ErrorManager } from "./shared/error-manager.js";
+import { exportContractToJSON, generateContractTree } from "./tree-builder.js";
 
 export function applyAnalysis(transformedFile: string, errorManager: ErrorManager): IRContract {
   const project = new Project({
