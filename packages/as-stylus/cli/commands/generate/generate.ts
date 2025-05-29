96450fdc8624bc1539f9ea23297ec70b126ab12d
francoperez03 <franco.perez03@gmail.com>
2025-05-29 13:11:57 -0300

    Update generate.ts

--- packages/as-stylus/cli/commands/generate/generate.ts
+++ packages/as-stylus/cli/commands/generate/generate.ts
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const baseContractsPath = path.resolve(__dirname, "../../../../../contracts");
+const baseContractsPath = path.resolve(__dirname, "../../../../contracts");
 
 const contractName = process.argv[2];
 
