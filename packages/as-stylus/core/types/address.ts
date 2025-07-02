289ccb7bbcc70e24fa08128f0905911a271f0043
juandahl <juandahl20@gmail.com>
2025-07-02 10:40:37 -0300

    refactor: create constant

--- packages/as-stylus/core/types/address.ts
+++ packages/as-stylus/core/types/address.ts
@@ -3,13 +3,13 @@
  ******************************************************************/
 import { malloc } from "../modules/memory";
 
-const ADDRESS_SIZE = 32;
-
 export class Address {
+  static ADDRESS_SIZE: u32 = 32;
+
   /*──────── helpers ────────*/
   static create(): usize {
     const ptr = malloc(32);
-    for (let i = 0; i < ADDRESS_SIZE; ++i) store<u8>(ptr + i, 0);
+    for (let i: u32 = 0; i < Address.ADDRESS_SIZE; ++i) store<u8>(ptr + i, 0);
     return ptr;
   }
 
@@ -18,17 +18,17 @@ export class Address {
     if (len >= 2 && load<u8>(str) == 0x30 && (load<u8>(str + 1) | 0x20) == 0x78) off = 2;
 
     const nibs: u32 = len - off;
-    const start = ADDRESS_SIZE - ((nibs + 1) >>> 1);
+    const start = Address.ADDRESS_SIZE - ((nibs + 1) >>> 1);
 
-    for (let i = 0; i < ADDRESS_SIZE; ++i) store<u8>(dest + i, 0);
+    for (let i: u32 = 0; i < Address.ADDRESS_SIZE; ++i) store<u8>(dest + i, 0);
 
-    let d: i32 = 19;
-    let s: i32 = <i32>(off + nibs - 1);
+    let d: u32 = 19;
+    let s: u32 = <u32>(off + nibs - 1);
 
     if (nibs & 1) {
       store<u8>(dest + d--, hexChar(load<u8>(str + s--)));
     }
-    while (d >= start && s >= <i32>off + 1) {
+    while (d >= start && s >= <u32>off + 1) {
       const low = hexChar(load<u8>(str + s));
       const high = hexChar(load<u8>(str + s - 1));
       store<u8>(dest + d--, (high << 4) | low);
@@ -37,14 +37,14 @@ export class Address {
   }
   static fromBytes(ptr_20: usize): usize {
     const addr = Address.create();
-    for (let i: i32 = 0; i < 32; ++i) {
+    for (let i: u32 = 0; i < Address.ADDRESS_SIZE; ++i) {
       store<u8>(addr + i, load<u8>(ptr_20 + i));
     }
     return addr;
   }
 
   static equals(a: usize, b: usize): boolean {
-    for (let i: usize = 0; i < ADDRESS_SIZE; ++i) {
+    for (let i: u32 = 0; i < Address.ADDRESS_SIZE; ++i) {
       const a_i = load<u8>(a + i);
       const b_i = load<u8>(b + i);
       if (a_i !== b_i) {
@@ -55,7 +55,7 @@ export class Address {
   }
 
   static isZero(ptr_address: usize): boolean {
-    for (let i: i32 = 0; i < ADDRESS_SIZE; ++i) {
+    for (let i: u32 = 0; i < this.ADDRESS_SIZE; ++i) {
       if (load<u8>(ptr_address + i) != 0) {
         return false;
       }
@@ -71,13 +71,13 @@ export class Address {
 
   static topic(addr: usize): usize {
     const t = malloc(32);
-    for (let i = 0; i < ADDRESS_SIZE; ++i) store<u8>(t + i, load<u8>(addr + i));
+    for (let i: u32 = 0; i < this.ADDRESS_SIZE; ++i) store<u8>(t + i, load<u8>(addr + i));
     return t;
   }
 
   static copyNew(src: usize): usize {
     const dst = this.create();
-    for (let i = 0; i < ADDRESS_SIZE; ++i) {
+    for (let i: u32 = 0; i < this.ADDRESS_SIZE; ++i) {
       store<u8>(dst + i, load<u8>(src + i));
     }
     return dst;
